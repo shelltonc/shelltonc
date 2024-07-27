3bdc5d2d7839fde9c91ba187a8b72ca473277658
shellton caetano <shell.rzo2008@gmail.com>
2024-07-27 00:51:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function teste() {
+//var userInput = document.getElementById("userInput").value;
+   document.getElementById("valor").innerHTML = "Parabéns, você é uma programador de sucesso"
+
+
+}
